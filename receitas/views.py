c07e1350e94a935ad0a8f3b1b67419f3944bb0a6
Phelipe com Ph <phelipecomph42@gmail.com>
2020-07-17 15:48:22 -0300

    Aula 4
    Cards no index feitos de forma dinamica
    Dados passados para dentro do index atraves do views
    servidor criado com postgresql e acesso configurado no settings.py
    modulo para ponte entre django e postgres instalado (psycopg2)
    model Receita foi criado
    migração do model e de outras pendendencias foram feitas (makemigrations, migrate) [commit, push]

--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 #Definindo qual é a resposta para a request index criada no arquivo urls.py
 def index(request):
-    return render(request,'index.html') #renderizando a pagina templates/index.html
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de Legumes',
+        3: 'Sorvete',
+        4: 'Bolo de Chocolate'
+    }
+
+    dados = {
+        'nome_das_receitas' : receitas
+    }
+
+    return render(request,'index.html', dados) #renderizando a pagina templates/index.html
 
 def receita(request):
     return render(request,'receita.html')
