9b1bba56c82924d0e0aa19ca3f7e5e5a5b391a19
Phelipe com Ph <phelipecomph42@gmail.com>
2020-07-17 16:55:27 -0300

    Aula 5
    model Receita importada e registrada em admin.py
    superuser(admin) criado com o manage.py
    
    dict removido do views e cards do index dinamicos a partir das infos do banco de dados
    
    dados da receita passada no url (alterações em urls, views, index.html, receita.html)

--- receitas/views.py
+++ receitas/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+
+from .models import Receita
 
 #Definindo qual é a resposta para a request index criada no arquivo urls.py
 def index(request):
-    receitas = {
-        1: 'Lasanha',
-        2: 'Sopa de Legumes',
-        3: 'Sorvete',
-        4: 'Bolo de Chocolate'
-    }
+    receitas = Receita.objects.all()
 
     dados = {
-        'nome_das_receitas' : receitas
+        'receitas' : receitas
     }
 
-    return render(request,'index.html', dados) #renderizando a pagina templates/index.html
+    return render(request, 'index.html', dados) #renderizando a pagina templates/index.html
 
-def receita(request):
-    return render(request,'receita.html')
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+    receita_a_exibir = {
+        'receita': receita
+    }
+    return render(request, 'receita.html', receita_a_exibir)
